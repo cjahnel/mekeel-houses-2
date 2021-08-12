a80de51094a76458ee3723b46b7e6429a685c0b2
Christian Jahnel <cjahnel@jahnelgroup.com>
2021-08-12 14:54:26 -0400

    Made Form better

--- src/app/main/house-card/add-points-dialog/add-points-dialog.component.ts
+++ src/app/main/house-card/add-points-dialog/add-points-dialog.component.ts
@@ -1,9 +1,10 @@
 import { Component, Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { House } from '../../main.component';
 
 export interface DialogData {
-  custom: string
+  house: House;
 }
 
 @Component({
@@ -13,20 +14,25 @@ export interface DialogData {
 })
 export class AddPointsDialogComponent {
   pointsRubricUrl = "https://docs.google.com/spreadsheets/d/1_BdWMlyx3JnN3fTEHw3xMeLkIAkM7-04/edit?usp=sharing&ouid=112595188143955831640&rtpof=true&sd=true";
-  form = new FormGroup({
-    date: new FormControl('', Validators.required),
-    amount: new FormControl('', Validators.required),
-    studentName: new FormControl('', Validators.required),
-    studentGrade: new FormControl('', Validators.required),
-    note: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(100)]),
-    house: new FormControl('', Validators.required),
-    teacherName: new FormControl('', Validators.required)
-  });
+  form: FormGroup;
+  house: House;
 
   constructor(
     public dialogRef: MatDialogRef<AddPointsDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
-  ) { }
+  ) {
+    this.house = data.house;
+
+    this.form = new FormGroup({
+      date: new FormControl('', Validators.required),
+      amount: new FormControl('', Validators.required),
+      studentName: new FormControl('', Validators.required),
+      studentGrade: new FormControl('', Validators.required),
+      note: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(100)]),
+      house: new FormControl({ value: this.house.name, disabled: true }, Validators.required),
+      teacherName: new FormControl('', Validators.required)
+    });
+  }
 
   // ngOnInit(): void {
   // }
